677f8c197951a672017a66a3b86d908413820c1c
root <alestrzelec@gmail.com>
2018-01-03 19:35:10 +0100

    render poll question one by one

--- src/frontend/src/components/FormSheet.js
+++ src/frontend/src/components/FormSheet.js
@@ -8,6 +8,7 @@ export default class FormSheet extends Component{
         super(props);
         this.state = {
             data: {},
+            counter: 0,
         }
     }
 
@@ -22,42 +23,82 @@ export default class FormSheet extends Component{
         this.props.onSubmit(this.state.data)
     }
 
-    render() {
+    nextQuestion(){
+        if(this.state.counter >= 0 && this.state.counter < this.props.pollData.questions.length){
+            this.setState({
+                counter: this.state.counter + 1
+            })
+        }
+        console.log('next' + this.state.counter)
+    }
+
+    prevQuestion(){
+        if(this.state.counter > 0 && this.state.counter < this.props.pollData.questions.length){
+            this.setState({
+                counter: this.state.counter - 1,
+            })
+        }
+        console.log('prev' + this.state.counter)
+
+    }
+
+
+    renderComponent(component) {
+        const shouldShowButtons = this.state.counter !== this.props.pollData.questions.length-1
         return (
             <form action="" onSubmit={e => this.letsSubmit(e)}>
-                {this.props.pollData.questions.map(elem => {
-                    const name = elem.id.toString()
-                    if (elem.type === 'score') {
-                        return (
-                            <PollScore
-                                questionText={elem.text}
-                                name={name}
-                                inputValue={this.state.data[name]}
-                                onChange={value => {this.setValue(name, value)}}
-                            />
-                        )
-                    } else if (elem.type === 'text') {
-                        return (
-                            <PollInput
-                                questionText={elem.text}
-                                inputValue={this.state.data[name]}
-                                onChange={value => {this.setValue(name, value)}}
-                            />
-                        )
-                    } else if (elem.type === 'options') {
-                        return (
-                            <PollOptionField
-                                questionText={elem.text}
-                                options={elem.options}
-                                inputValue={this.state.data[name]}
-                                onChange={value => {this.setValue(name, value)}}
-                            />
-                        )
-                    }
-                })}
-                <input type="submit" value="Submit"/>
+                <div>
+                    {component}
+                    {shouldShowButtons &&
+                        <div>
+                            <button onClick={() => this.prevQuestion()}>PREV</button>
+                            <button onClick={() => this.nextQuestion()}>NEXT</button>
+                        </div>}
+                </div>
             </form>
         )
     }
-}
 
+    render() {
+        if (this.state.counter < this.props.pollData.questions.length-1) {
+            const elem = this.props.pollData.questions[this.state.counter]
+            const name = elem.id.toString()
+            switch (elem.type) {
+                case 'score':
+                    return this.renderComponent(
+                        <PollScore
+                            questionText={elem.text}
+                            name={name}
+                            inputValue={this.state.data[name]}
+                            onChange={value => {this.setValue(name, value)}}
+                        />
+                    )
+                case 'text':
+                    return this.renderComponent (
+                        <PollInput
+                            questionText={elem.text}
+                            inputValue={this.state.data[name]}
+                            onChange={value => {this.setValue(name, value)}}
+                        />
+                    )
+                case 'options':
+                    return this.renderComponent(
+                        <PollOptionField
+                            questionText={elem.text}
+                            options={elem.options}
+                            inputValue={this.state.data[name]}
+                            onChange={value => {this.setValue(name, value)}}
+                        />
+                    )
+            }
+        } else if(this.state.counter === this.props.pollData.questions.length-1) {
+            return this.renderComponent(
+                <div>
+                    <p>Dziękujemy za wypełnienie ankiety</p>
+                    <button onClick={() => this.prevQuestion()}>PREV</button>
+                    <input type="submit" value='Submit'/>
+                </div>
+            )
+        }
+    }
+}
