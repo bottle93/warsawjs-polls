de444e318c30afcc89fab827c1c3a75eb24b37d0
root <alestrzelec@gmail.com>
2018-01-10 19:44:09 +0100

    routing added

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import FormSheet from './components/FormSheet';
-import Navigation from './components/Navigation';
 import axios from 'axios';
 
 
