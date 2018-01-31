daef96e0147f469c09e52c6e9d960bfbb631f5dc
Valian <jakub.skalecki@gmail.com>
2018-01-31 12:19:26 +0100

    staticfiles

--- src/backend/poll/settings.py
+++ src/backend/poll/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
+STATIC_ROOT = '/static/'
 
 LOGGING = {
     'version': 1,
